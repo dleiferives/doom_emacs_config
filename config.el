5f20cbe4c5cb5aaedec21d80476fdc311d53addd
Dylan Leifer-Ives <dleiferives@gmail.com>
2025-01-24 23:55:06 -0800

    doom emacs config feeling okay

---
+++ config.el
@@ -0,0 +1,172 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets. It is optional.
+;; (setq user-full-name "John Doe"
+;;       user-mail-address "john@doe.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom:
+;;
+;; - `doom-font' -- the primary font to use
+;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
+;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;; - `doom-symbol-font' -- for symbols
+;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
+;;
+;; See 'C-h v doom-font' for documentation and more examples of what they
+;; accept. For example:
+;;
+;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
+;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
+;;
+;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
+;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
+;; refresh your font settings. If Emacs still can't find your font, it likely
+;; wasn't installed correctly. Font issues are rarely Doom issues!
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'custom_light)
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type t)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/org/")
+
+
+;; Whenever you reconfigure a package, make sure to wrap your config in an
+;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
+;;
+;;   (after! PACKAGE
+;;     (setq x y))
+;;
+;; The exceptions to this rule:
+;;
+;;   - Setting file/directory variables (like `org-directory')
+;;   - Setting variables which explicitly tell you to set them before their
+;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
+;;   - Setting doom variables (which start with 'doom-' or '+').
+;;
+;; Here are some additional functions/macros that will help you configure Doom.
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
+;; etc).
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
+(defun my-weebery-is-always-greater ()
+  (let* ((banner '("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠀⠀⠀⠒⠒⠢⢌⠳⡤⠑⡥⢿⣦⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+                   "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⣠⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⠀⠱⠀⠘⠲⢧⢙⠛⠄⠠⠆⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+                   "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠔⢣⣴⠖⢀⡀⠀⠀⣌⠳⡆⠉⠁⠀⢤⡐⢄⠀⠀⠃⡇⢰⠰⠆⡘⢀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+                   "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠶⣿⢷⣶⣿⣷⣦⣄⠉⠁⠉⠉⠉⠢⠀⠀⠀⠈⠁⠀⠁⠀⠀⠁⠁⠀⠘⠀⠁⠀⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+                   "⠀⠀⠀⠀⠀⠀⠀⠀⠠⡊⠁⠀⠉⢹⣿⣶⣄⢉⣁⠀⠀⠀⠶⠠⠌⠃⠀⠀⠒⠀⠀⠀⠀⠀⠰⠀⢤⡄⠀⠀⠸⠤⢤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+                   "⠀⠀⠀⠀⠀⠀⢠⠴⠺⠁⠀⠀⠀⠘⠞⠛⠋⠊⠁⡠⠒⢀⠀⠀⠀⠀⢀⠀⠀⡀⠀⠀⠀⠀⠀⠀⠀⠈⠂⣠⣶⠀⠣⡉⠂⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+                   "⠀⠀⠀⠀⢀⣀⣠⠀⠀⠀⠀⠀⢀⣄⠀⠀⢀⠀⡊⠀⠀⠈⠀⠀⢠⣀⣀⣴⣾⣆⠀⠀⠀⠘⠀⠀⣡⡶⣄⠹⠃⠀⠀⠐⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+                   "⠀⠀⠀⠀⠀⡟⠀⠀⠀⠀⠐⠤⣀⢽⣶⡿⠋⣿⠓⠂⠀⠀⠀⣸⠀⠉⠻⢭⣫⠽⣷⠊⠐⠒⠀⠀⠻⡵⣣⢸⠀⠀⢠⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+                   "⠀⠀⢀⡴⢋⠄⠀⠀⠀⠀⠀⣸⡅⢘⠼⠀⠈⣯⠒⠁⠀⠀⠀⠀⠁⠀⡤⠀⠀⠈⠀⣈⣁⠀⠀⡀⠀⠀⠉⠺⠄⠀⡞⠘⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+                   "⠀⠀⠁⣴⣾⠀⠀⠀⠀⠀⢀⣽⣿⢸⠐⠀⠀⠹⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠀⠻⣣⣤⣤⣴⣬⢠⠀⠀⠡⣠⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+                   "⠀⠀⠰⣿⠊⠀⠀⠀⠀⠀⠀⣀⢻⡾⡆⠂⠀⠐⢛⣀⠀⠀⠀⠀⠀⠀⠀⢀⣤⣷⣤⣀⡀⠀⠀⠀⠀⠉⢿⠋⠁⢸⠀⠀⠀⠀⠈⡏⠓⠢⣤⣀⡀⠀⠀⠀"
+                   "⠀⠀⠀⣟⠔⠀⠀⠀⠀⠀⠀⠀⠀⣷⠃⡀⠀⠀⠋⠩⡂⠀⠀⠀⠀⢤⣾⣿⣿⣿⣿⣿⣿⣦⡤⣠⣴⣶⣾⣶⣶⣿⣇⠀⠀⢀⣶⡶⢀⡒⠰⡄⠉⠓⠶⠤"
+                   "⠀⠀⣀⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⠀⡠⠀⠀⠀⠀⢱⠀⠀⠀⣠⠞⣿⠀⠀⠈⠉⠨⣿⣾⣷⡿⠛⣿⡿⢿⣏⣉⡀⠈⣱⣾⣷⣿⣾⠇⣰⡇⢠⡀⠀⠀"
+                   "⠀⢀⡅⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⠟⠀⠀⢰⠂⠀⢸⡀⠠⠞⠁⠀⢿⠀⠀⠀⢶⠖⠉⢉⣩⠄⠀⣿⣴⣿⣿⣿⣿⣿⣯⡛⢿⠿⣃⣸⣿⣷⣿⠃⣶⡀"
+                   "⣾⠇⠀⠀⠀⠀⣀⣠⣤⡤⣄⡤⠚⠛⠉⠉⠢⡔⢦⡄⠀⡇⠀⠀⠀⢠⣼⠀⠀⠜⠀⣠⣤⠉⠈⣻⡟⣿⣿⣿⣿⣿⣿⣿⣿⣍⠓⢮⡛⢿⣿⠟⣡⣾⣿⣿"
+                   "⣁⡤⠤⠖⠒⣋⣉⣹⡿⠟⠋⠉⠉⠐⢄⢐⢠⣼⡞⢀⡰⠃⠀⠀⣼⣿⣯⡀⠀⠀⢼⠟⠉⣩⣿⠟⢻⣿⣿⣿⣿⣿⣿⣿⣿⣀⠤⠤⠉⠓⢖⡺⢿⣿⣿⣿"
+                   "⡁⠀⠀⢀⣀⡴⠿⠥⠀⠠⢀⠀⢀⠔⣆⢹⣶⣿⡵⠋⠀⠀⠀⠀⣿⣿⣽⣿⣶⣆⠀⠒⠛⣻⣿⢆⣿⣿⣿⣿⡿⠛⢉⣩⠤⠤⠦⠤⠄⠀⠀⠈⠢⣍⡋⣥"
+                   "⢶⣶⡞⣉⠀⠀⠀⠀⠀⢈⠀⡍⠒⣧⣸⣆⡏⣸⠁⠀⠀⠀⠀⣠⣿⣿⡅⠹⣿⣿⣿⣿⣿⡿⣿⡯⣏⠉⣿⡳⠻⠋⠛⠑⠒⠂⠒⠠⠀⠠⣤⣠⣄⣌⠑⢤"
+                   "⠡⠀⠀⠀⠀⠀⠀⠀⡴⣁⠘⢴⣦⠸⣿⢿⣴⢧⣀⡤⣺⣿⢰⣿⡟⠆⠀⣀⣙⠻⣿⣟⡈⠰⣿⠁⠈⠳⣼⣠⣁⠀⠀⢳⡄⠀⠁⠀⡀⠀⠀⠀⠀⣀⣾⣶"
+                   "⠀⠀⠀⠠⠂⣐⢄⠀⢙⣿⣆⣸⣿⣧⠏⣾⣷⣶⡟⠉⠉⣿⡟⠛⠉⠁⠀⠀⠀⠉⠻⡋⠀⠀⢨⣀⣀⣀⣹⣛⡿⠁⠀⢨⠀⠀⠀⠀⠁⠀⣀⣠⣴⣿⣿⣿"
+                   "⠀⠀⠀⠀⠐⠛⢦⠹⣿⣿⣿⣿⡿⢻⣾⡁⢻⣿⣿⣦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠛⢻⣿⠥⠴⢳⣶⢠⠁⢀⣴⣾⣷⣷⣾⣿⣿⣿⣿⣿⣿"
+                   "⠞⠁⠀⠀⠀⣠⣿⣷⡸⣿⣿⠟⢀⡾⢹⡇⢸⣿⣿⣿⡇⠀⠀⠀⣀⠀⠀⠀⠐⠀⢚⠿⠟⣛⣛⣭⣤⣖⡀⡟⠛⠃⠁⠀⣴⢿⣭⣿⣦⣤⣭⣭⣭⡿⠿⠛"))
+         (longest-line (apply #'max (mapcar #'length banner))))
+    (put-text-property
+     (point)
+     (dolist (line banner (point))
+       (insert (+doom-dashboard--center
+                +doom-dashboard--width
+                (concat line (make-string (max 0 (- longest-line (length line))) 32)))
+               "\n"))
+     'face 'doom-dashboard-banner)))
+
+(setq +doom-dashboard-ascii-banner-fn #'my-weebery-is-always-greater)
+
+
+
+(when (string-match "-[Mm]icrosoft" operating-system-release)
+  ;; WSL: WSL1 has "-Microsoft", WSL2 has "-microsoft-standard"
+ (defun wsl-copy-clip (&rest _args)
+   "Copy current-kill to WSL clip.exe"
+   (setq mytemp (make-temp-file "winclip"))
+   (write-region (current-kill 0 t) nil mytemp)
+   (shell-command (concat "clip.exe < " mytemp))
+   (delete-file mytemp))
+
+ (advice-add 'kill-new :after #'wsl-copy-clip))
+
+(add-to-list 'auto-mode-alist '("\\.d\\'" . c-mode))
+
+(defun my-d-mode-hook ()
+  (font-lock-add-keywords nil
+   '(("\\(@[a-zA-Z_][a-zA-Z0-9_]*\\)(" 1 font-lock-function-name-face))))
+
+(add-hook 'c-mode-hook 'my-d-mode-hook)
+
+(after! lsp-clangd
+  (setq lsp-clients-clangd-args
+        '("-j=3"
+          "--background-index"
+          "--clang-tidy"
+          "--completion-style=detailed"
+          "--header-insertion=never"
+          "--header-insertion-decorators=0"))
+  (set-lsp-priority! 'clangd 2))
+
+(add-hook 'c-mode-hook
+          (lambda ()
+            (setq comment-start "// "
+                  comment-end "")))
+(map! :leader
+      (:prefix ("l" . "custom comments")
+       :desc "Add TODO comment" "t" #'add-todo-comment
+       :desc "Add NOTE comment" "n" #'add-note-comment
+       :desc "Add WARN comment" "w" #'add-warn-comment))
+
+(defun add-comment (prefix)
+  (let* ((comment-start (or comment-start "# "))
+         (content (read-string (concat prefix ": ")))
+         (comment-string (concat comment-start prefix " @(dleiferives): " content)))
+    (save-excursion
+      (beginning-of-line)
+      (open-line 1)
+      (insert comment-string))))
+
+(defun add-todo-comment ()
+  (interactive)
+  (add-comment "TODO"))
+
+(defun add-note-comment ()
+  (interactive)
+  (add-comment "NOTE"))
+
+(defun add-warn-comment ()
+  (interactive)
+  (add-comment "WARN"))
